720cb1fa55ffbde380baf6f759ad7c1fa786626a
NaifAliAli <n.aljarahi@tabweeb.com>
2026-07-28 03:05:57 +0300

    Add files via upload

---
+++ simple/templates-manifest.js
@@ -0,0 +1,13 @@
+﻿// يُحدَّث تلقائياً عند تشغيل convert-templates.ps1
+// أضف صورة PNG/JPG إلى مجلد templates/ ثم شغّل السكربت
+const templatesManifest = [
+    { name: "template1", path: "templates/template1.png" },
+    { name: "template2", path: "templates/template2.jpeg" },
+    { name: "template3", path: "templates/template3.jpeg" },
+    { name: "template4", path: "templates/template4.jpeg" },
+    { name: "template5", path: "templates/template5.jpeg" },
+    { name: "template6", path: "templates/template6.jpeg" },
+    { name: "template7", path: "templates/template7.jpeg" },
+    { name: "template8", path: "templates/template8.jpeg" },
+    { name: "template9", path: "templates/template9.jpeg" }
+];
